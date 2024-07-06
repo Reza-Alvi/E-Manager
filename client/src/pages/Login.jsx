c5a392612720b512a11c97b59bee7411b1204cd4
Akib-Mahmud-Omio <152496298+Akib-Mahmud-Omio@users.noreply.github.com>
2024-07-07 03:46:00 +0600

    Update Login Vercel

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
         }
         setIsLoading(true);
         try {
-            const url = "http://localhost:8080/auth/login";
+            const url = "https://e-manager-api.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
