c0e5eebb525a950ede5a0d79ca77e037973c4191
Reza066 <reza.cse.20220104066@aust.edu>
2024-09-16 21:30:31 +0600

    Access token generation fixed

---
+++ client/src/pages/utils/axiosInstance.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://e-manager-api.vercel.app', // your API base URL
+});
+
+// Request interceptor to add the access token to headers
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Response interceptor to handle token refresh logic
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 403 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+                const response = await axiosInstance.post('/auth/refresh-token', { refreshToken });
+                if (response.status === 200) {
+                    const { accessToken } = response.data;
+                    localStorage.setItem('accessToken', accessToken);
+                    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+                    originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+                    return axiosInstance(originalRequest);
+                }
+            } catch (refreshError) {
+                if ( refreshError.response.status === 401) {
+                    // Refresh token is expired
+                    localStorage.removeItem('accessToken');
+                    localStorage.removeItem('refreshToken');
+                    localStorage.removeItem('loggedInUser');
+                    window.location.href = '/login'; // Redirect to login page
+                }
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
