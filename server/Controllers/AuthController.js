35c1758aa7914c6d6e6fcec667646804e95fff28
Reza066 <reza.cse.20220104066@aust.edu>
2024-09-20 20:39:23 +0600

    Signup Error Fixed

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -12,12 +12,12 @@ const generateRefreshToken = (user) => {
 
 const signup = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const { firstName, lastName, dateOfBirth, gender, phoneNumber, email, password } = req.body;
         const user = await UserModel.findOne({ email });
         if (user) {
             return res.status(409).json({ message: 'User already exists, you can login', success: false });
         }
-        const userModel = new UserModel({ name, email, password });
+        const userModel = new UserModel({ firstName, lastName, dateOfBirth, gender, phoneNumber, email, password });
         userModel.password = await bcrypt.hash(password, 10);
         await userModel.save();
         res.status(201).json({ message: 'Signup successfully', success: true });
@@ -26,6 +26,7 @@ const signup = async (req, res) => {
         res.status(500).json({ message: 'Internal server error', success: false });
     }
 };
+
 
 const login = async (req, res) => {
     try {
