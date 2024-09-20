e4af9cb97cbea5a36039a7dc75bc9d4e8e236a13
Akib Mahmud Omio <assassinscreed.telenor@gmail.com>
2024-09-20 22:15:31 +0600

    Forgot Password + Reset Password Backend

--- server/Routes/AuthRouter.js
+++ server/Routes/AuthRouter.js
@@ -1,4 +1,4 @@
-const { signup, login, getProfile, updateProfile, refreshAccessToken } = require('../Controllers/AuthController');
+const { signup, login, getProfile, updateProfile, forgotPassword, resetPassword, refreshAccessToken } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 const ensureAuthenticated = require('../Middlewares/Auth');
 const multer = require('multer');
@@ -14,5 +14,7 @@ router.post('/refresh-token', refreshAccessToken);
 router.get('/profile', ensureAuthenticated, getProfile);
 router.put('/profile', ensureAuthenticated, updateProfile);
 router.put('/profile-details', ensureAuthenticated, upload.single('profilePicture'), updateProfile);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
