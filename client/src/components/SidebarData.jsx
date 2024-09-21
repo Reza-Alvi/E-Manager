ddf2b678db468e85b73b1eff3eb78d52dbf29e66
Akib-Mahmud-Omio <152496298+Akib-Mahmud-Omio@users.noreply.github.com>
2024-09-21 13:33:59 +0600

    Revert "Last fixing"
    This reverts commit e837554af8cbac62b65c0a5877732c0d9db3dd61.

--- client/src/components/SidebarData.jsx
+++ client/src/components/SidebarData.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
-import Graph from '../pages/Graph';
 
 export const SidebarData = [
   {
